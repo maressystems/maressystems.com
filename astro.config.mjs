8a4f6eb92675e2ad4d9161e48fb5fe124ec9bc57
fi-ma <filip.mares@gmx.at>
2024-02-21 14:13:32 +0100

    add vercel integration and plausible js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import tailwind from '@astrojs/tailwind'
-import compress from 'astro-compress'
-import icon from "astro-icon"
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
+import icon from "astro-icon";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
   integrations: [mdx(), icon(), tailwind({
-    applyBaseStyles: false,
+    applyBaseStyles: false
   }), compress()],
-})
+  site: 'https://maressystems.com',
+  adapter: vercel()
+});
