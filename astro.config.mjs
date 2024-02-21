41df004047a0a992f11be2371042b916a4d40923
fi-ma <filip.mares@gmx.at>
2024-02-21 14:21:30 +0100

    remove astro-compress package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
-import compress from 'astro-compress';
 import icon from "astro-icon";
 import vercel from "@astrojs/vercel/serverless";
 
@@ -10,7 +9,7 @@ export default defineConfig({
   compressHTML: true,
   integrations: [mdx(), icon(), tailwind({
     applyBaseStyles: false
-  }), compress()],
+  })],
   site: 'https://maressystems.com',
   adapter: vercel()
 });
